835d5a2573464d5192f399a0e50ca6b73c30fde4
jayant818 <jayant2@s.amity.edu>
2025-05-01 02:29:18 +0530

    Refactor react-hook-form import in try page

--- frontend/app/try/page.tsx
+++ frontend/app/try/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 // import { useForm } from "react-hook-form";
-import z from "zod";
+// import z from "zod";
 // import { zodResolver } from "@hookform/resolvers/zod";
 import { Triangle } from "react-loader-spinner";
 import { useGetReviewMutation } from "@/src/hooks/api-hooks/useReviewQuery";
